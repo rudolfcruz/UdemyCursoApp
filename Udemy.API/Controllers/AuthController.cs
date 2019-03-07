52bd4d8208e25f4bfb4a1891f0e4f46fe6195b7f
Ing. Rudolf <ico_rudolf@hotmail.com>
2019-03-06 23:49:57 -0600

    Se agrega DTo prueba y validacion de registro de usuario

---
+++ Udemy.API/Controllers/AuthController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Udemy.API.Data;
+using Udemy.API.Dtos;
+using Udemy.API.Models;
+
+namespace Udemy.API.Controllers
+{
+    [Route("api/[controller]")]
+    //[ApiController]
+    public class AuthController : ControllerBase
+    {
+		private readonly IAuthRepository _repository;
+
+		public AuthController(IAuthRepository repository)
+		{
+			_repository = repository;
+		}
+
+		[HttpPost("register")]
+		public async Task<IActionResult> Register([FromBody]UserForRegisterDto userForRegisterDto)
+		{
+			//validate request
+			if (!ModelState.IsValid)
+				return BadRequest(ModelState);
+
+			userForRegisterDto.Username = userForRegisterDto.Username.ToLower();
+
+			if (await _repository.UserExist(userForRegisterDto.Username))
+				return BadRequest("Username Already exists");
+
+			var userToCreate = new User {
+
+				Username = userForRegisterDto.Username
+			};
+
+			var createdUser = await _repository.Register(userToCreate, userForRegisterDto.Password);
+			return StatusCode(201);
+		}
+    }
+}
--- Udemy.API/Startup.cs
+++ Udemy.API/Startup.cs
@@ -31,6 +31,7 @@ namespace Udemy.API
             services.AddDbContext<DataContext>(options => options.UseSqlite(Configuration.GetConnectionString("EntitiesTest")));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
             services.AddCors();
+			services.AddScoped<IAuthRepository, AuthRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
